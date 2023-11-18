c426fe86a28a66bdc16eda2769ee0d03e9ce478b
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2023-11-18 19:20:11 -0300

    creada conexion a mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(configuration().mongo_atlas)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
