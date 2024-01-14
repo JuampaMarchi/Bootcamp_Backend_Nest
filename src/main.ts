35ee69ed12c11394dbdcafb093d1bdf1453126ec
Juampa <juanpablomarchi@gmail.com>
2024-01-14 14:47:14 -0300

    instalacion swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import configuration from './config/configuration';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+//Variables de entorno
+import env from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(configuration().port);
+
+  const config = new DocumentBuilder()
+    .setTitle('Proyecto NestJS Codigo Facilito')
+    .setDescription('Proyecto de Blog con posteos y comentarios')
+    .setVersion('1.0')
+    .addTag('NestJS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document)
+
+  await app.listen(env().port);
 }
 
 bootstrap();
